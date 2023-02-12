bdacdde604d9cf9c9c6dcb4316a63cb1d1591696
stasdmitr <dmitrive2010>
2023-02-13 00:34:11 +0300

    Продолжаю дорабатывать сайт

--- D6/D6/urls.py
+++ D6/D6/urls.py
@@ -1,10 +1,11 @@
-
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
-   path('admin/', admin.site.urls),
-   path('pages/', include('django.contrib.flatpages.urls')),
-   path('', include('simpleapp.urls')),
+    path('admin/', admin.site.urls),
+    path('pages/', include('django.contrib.flatpages.urls')),
+    path('', include('simpleapp.urls')),
+    path('protect/', include('protect.urls')),
+    path('sign/', include('sign.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
